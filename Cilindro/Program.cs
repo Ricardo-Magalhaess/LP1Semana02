aa9619e6590645f8161055d01b3d8859a4298c50
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-03-01 14:21:49 +0000

    foi criado um programa onde pede a altura e o volume para um cilindro e no final apresenta o volume do cilindro e a area do cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indique uma altura para um cilindro: ");
+            string alt = Console.ReadLine();
+            Console.Write("Indique um raio para um cilindro: ");
+            string ra = Console.ReadLine();
+            float a = float.Parse(alt);
+            float r = float.Parse(ra);
+            float v = MathF.PI * MathF.Pow(r,2) * a;
+            float s = 2 * MathF.PI * r * (r+a);
+            Console.WriteLine($"Volume do cilindro {v}");
+            Console.WriteLine($"area da superficie do cilindro {s}");      
+        }
+    }
+}
