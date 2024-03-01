9fed59b6f2390f047119671198567bd0f5dd372a
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-03-01 13:53:45 +0000

    foram adicionadas instrucoes de operacoes em n para divisao por2, shift left por 3, xor com 5 e se é maior que 10

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -11,6 +11,10 @@ namespace SmoothOperator
             byte n = byte.Parse(negro);
             Console.WriteLine(--n);
             Console.WriteLine(++n);
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^5);
+            Console.WriteLine(n>10);
         }
     }
 }
